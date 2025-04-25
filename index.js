04d1a993c4f626bc1c8da331d5ebbf1482602090
Samuel Kang <samuel.june.kang@gmail.com>
2025-04-25 00:51:20 -0700

    Fixed

--- index.js
+++ index.js
@@ -20,40 +20,50 @@ function createObject(operation, operand, output) {
     calculationHistory.push(newObject)
 }
 
-// if (typeof output === 'number' && !isNaN(output)) {
-//     console.log(output)
-// } else {
-//     console.log("invalid input!")
-// }
-
-function isANumber(value) {
-    return (typeof value === 'number' && !isNaN(true))
+//A global function: Checks if its a number or NaN
+function checkNumber(output) {
+    if (typeof output !== 'number' || isNaN(output)) {
+        return output = 'Invalid results'
+    } else {
+        return output
+    }
 }
 
+
 function add(num1, num2) {
-    const output = num1 + num2
+    let output = num1 + num2
+    output = checkNumber(output)
     createObject('addition', [num1, num2], output)
 }
 
 function subtract(num1, num2) {
-    const output = num1 - num2
+    let output = num1 - num2
+    output = checkNumber(output)
     createObject('subtraction', [num1, num2], output)
 }
 
 function multiply(num1, num2) {
-    const output = num1 * num2
+    let output = num1 * num2
+    output = checkNumber(output)
     createObject('multiplification', [num1, num2], output)
 }
 
 function divide(num, den) {
-    const output = num / den
+    let output = num / den
+    output = checkNumber(output)
     createObject('division', [num, den], output)
 }
 
+
+//testing functions
 // add(10, 2)
 // subtract(10, 2)
 // multiply(10, 2)
 // divide(10, 2)
+// divide(2, 'a')
+// divide(0, 1)
+// multiply(0,1)
+// multiply(0,0)
 
 
 checkArray(calculationHistory)
